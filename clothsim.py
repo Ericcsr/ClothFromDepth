c21bbcceac4b36223f99a3f1177637296553dce0
Chen Sirui <43209734+Ericcsr@users.noreply.github.com>
2021-12-18 23:50:06 +0800

    enable partially working simulator

--- clothsim.py
+++ clothsim.py
@@ -1,13 +1,15 @@
 from numpy.core.arrayprint import dtype_is_implied
+from numpy.lib.utils import _set_function_name
 import taichi as ti
 import open3d as o3d
 import numpy as np
 
-MAX_VERTICES = 2000
-MAX_FACES = 4000
+# TODO: Taichi Visualization need the field size to be actual particle number which limit flexibility
+MAX_VERTICES = 509
+MAX_FACES = 924
 # If a vertex connect more than 10 verticies then, the mesh construction has some problem
 MAX_NEIGHBOR = 10
-MAX_BODIES = 10
+MAX_BODIES = 1
 ELASTICITY = 0.4
 
 # Define Ball Types
@@ -24,7 +26,7 @@ class ClothSim:
         damping = 2.0,
         unit_length = 1.0,
         dt = 5e-4,
-        n_substeps = 30):
+        n_substeps = 50):
         self.gravity = gravity
         self.stiffness = stiffness
         self.damping = damping
@@ -54,6 +56,7 @@ class ClothSim:
         self.balls_v_inc = ti.Vector.field(3, float, MAX_BODIES)
         self.balls_r = ti.field(float, MAX_BODIES) # Should be float radius of ball
         self.balls_type = ti.field(ti.int32, MAX_BODIES)
+        self.render_initialized = False
         if init_mesh != None:
             self.load_mesh(init_mesh)
 
@@ -66,20 +69,23 @@ class ClothSim:
         success flag : bool 
         '''
         self.cloth_mesh = mesh
-        self.cloth_mesh.remove_duplicate_vertices()
+        self.cloth_mesh.remove_duplicated_vertices()
         self.cloth_mesh.compute_adjacency_list()
         links = [np.array(list(v)) for v in self.cloth_mesh.adjacency_list]
         faces = np.asarray(self.cloth_mesh.triangles)
         self.num_triangles = len(faces)
         assert(self.num_triangles <= MAX_FACES)
+        print(self.num_triangles)
         # Extrapolate
-        faces = np.vstack([faces, np.zeros(MAX_FACES-self.num_triangles, 3)])
+        faces = np.vstack([faces, np.zeros((MAX_FACES-self.num_triangles, 3))])
         self.triangles.from_numpy(faces)
         self.indices.from_numpy(faces.flatten())
 
         vertices = np.asarray(self.cloth_mesh.vertices)
+        vertices[:,0] += 0.4
         self.N = len(vertices)
         assert(self.N <= MAX_VERTICES)
+        print(self.N)
         # Extrapolate
         vertices = np.vstack([vertices, np.zeros((MAX_VERTICES - self.N, 3))])
         self.x.from_numpy(vertices)
@@ -89,11 +95,19 @@ class ClothSim:
         links_np = np.zeros((MAX_VERTICES, MAX_NEIGHBOR))
         links_len = np.array([len(link) for link in links])
         assert(np.max(links_len) <= MAX_NEIGHBOR)
+        links_len = np.hstack([links_len, np.zeros(MAX_VERTICES-self.N)])
         for i, link in enumerate(links):
             links_np[i, :len(link)] = link
         self.adj_list.from_numpy(links_np)
         self.adj_num.from_numpy(links_len)
         return True
+
+    def initialize_renderer(self):
+        self.window = ti.ui.Window("Cloth", (800, 800), vsync=True)
+        self.canvas = self.window.get_canvas()
+        self.scene = ti.ui.Scene()
+        self.camera = ti.ui.make_camera()
+        self.render_initialized = True
 
     def load_objects(self,centers, radius, collision_type):
         '''Load rigid bodies that can interact with clothes
@@ -105,8 +119,9 @@ class ClothSim:
         success_flag : bool
         '''
         assert(len(centers) == len(radius))
+        self.num_objects = len(centers)
         n_objects = len(centers)
-        centers = np.vstack([centers, np.zeros(MAX_BODIES - n_objects, 3)])
+        centers = np.vstack([centers, np.zeros((MAX_BODIES - n_objects, 3))])
         self.balls_x.from_numpy(centers)
         self.balls_x_rest.from_numpy(centers)
         self.balls_v.from_numpy(np.zeros_like(centers))
@@ -167,19 +182,22 @@ class ClothSim:
                 rest_pos = self.x_rest[self.adj_list[i, neighbor]] - self.x_rest[i]
                 rest_length = rest_pos.norm()
                 self.v[i] += (self.stiffness * self.unit_length / rest_length)\
-                             * relative_pos.normalized() * (current_length - rest_length)
+                             * relative_pos.normalized() * (current_length - rest_length) * self.dt
 
         for i in range(self.N):
             self.v[i] *= ti.exp(-self.damping * self.dt)
             for j in ti.static(range(self.num_objects)):
                 r = self.x[i] - self.balls_x[j]
-                if r.norm() < self.balls_r[j]:
+                if r.norm() <= self.balls_r[j]:
                     if self.balls_type[j] == BALL_STICK:
-                        self.v[i] = self.balls_v[j]
+                        self.v[i] = ti.Vector([0.0, 0.0, 0.0])
                     else:
-                        tang_v = self.v[i] - self.v[i].dot(r.normalized()) * r.normalized()
-                        norm_ball_v = self.balls_v[j].dot(r.normalized()) * r.normalized()
-                        self.v[i] = tang_v + norm_ball_v
+                        proj_v = self.v[i].dot(r.normalized())
+                        norm_v = proj_v * r.normalized()
+                        tang_v = self.v[i] - norm_v
+                        if proj_v <=0:
+                            norm_ball_v = self.balls_v[j].dot(r.normalized()) * r.normalized()
+                            self.v[i] = tang_v + norm_ball_v
             self.x[i] += self.dt * self.v[i]
                 
             
@@ -190,13 +208,34 @@ class ClothSim:
             self.balls_v[i] = self.balls_v[i] + self.balls_v_inc[i]
             self.balls_x[i] = self.balls_x[i] + self.dt * self.balls_v[i] + self.balls_x_inc[i]
 
-    def step(self,action):
-        assert(len(action) == self.num_objects)
-        for i in range(len(action)):
-            self.balls_v[i] = action[i]
+    def step(self,action=None):
+        """step by action
+        action : np.ndarray (3 * n_objects)
+        """
+        if action == None:
+            action = np.zeros(self.num_objects * 3)
+        assert(len(action)//3 == self.num_objects)
+        for i in range(self.num_objects):
+            self.balls_v[i] = action[3*i:3*(i+1)]
 
-        for s in range(self.num_substeps):
+        for _ in range(self.num_substeps):
             self.substep()
+
+    def render(self):
+        if not self.render_initialized:
+            print("Render Not initialized")
+            exit(-1)
+        self.camera.position(0.5, -0.5, 2)
+        self.camera.lookat(0.5, -0.5, 0)
+        self.scene.set_camera(self.camera)
+        self.scene.point_light(pos=(0.5, 1, 2), color=(1, 1, 1))
+        self.scene.mesh(self.x,
+                        indices = self.indices,
+                        color=(0.5, 0.5, 0.5),
+                        two_sided=True)
+        self.scene.particles(self.balls_x, radius = 0.1, color=(0.5, 0, 0))
+        self.canvas.scene(self.scene)
+        self.window.show()
 
     @ti.kernel
     def reset(self):
@@ -216,3 +255,16 @@ class ClothSim:
         assert(index < self.num_objects and (collision_type==1 or collision_type==2))
         self.balls_type[index] = collision_type
     
+if __name__ == "__main__":
+    ti.init(arch = ti.cpu)
+    clothmesh = o3d.io.read_triangle_mesh("./test2.obj")
+    sim = ClothSim(init_mesh=clothmesh)
+    sim.initialize_renderer()
+    centers = np.array([[0.5, -0.5, 0.0]])
+    radius = np.array([0.1])
+    type = np.array([BALL_SLIDE])
+    sim.load_objects(centers, radius, type)
+    for s in range(1000):
+        sim.step()
+        sim.render()
+
